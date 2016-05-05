7fe292b567b520a3aa1976610974df17b2ca8f87
relative-rene <rene.arellano415@gmail.com>
2016-05-05 15:23:03 -0700

    initial commit Review controller and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/show'
+
+  get 'reviews/update'
+
+  get 'reviews/destroy'
+
   root to: "home#index"
 
   get "/home", to: "home#index", as: "home"
