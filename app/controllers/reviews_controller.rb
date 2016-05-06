555447209fabe07325c7bcf4f6cb73480bcd9340
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-06 09:46:28 -0700

    changed back to original controller (removed some code to test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,45 +1,47 @@
 class ReviewsController < ApplicationController
   before_action :logged_in?, only: [:show, :create]
   before_action :get_id, only: [:show, :destroy, :edit, :update]
-  def index
-    @reviews = Review.all
-    render :index
+    def index
+      @reviews = Review.all
+      render :index
+    end
+
+    def new
+      @review = Review.new
+      render :new
+    end
+
+    def create
+      @review = Review.create(review_params)
+      login(@review)
+      redirect_to root_path
+      #double check this function
+    end
+
+    def show
+      render :show
+    end
+
+    def edit
+      render :edit
+    end
+
+    def update
+      review_params = get_review_params
+      review.update_attributes(review_params)
+      redirect_to show_review
+    end
+
+    def destroy
+      review.destroy
+      redirect_to root_path
+    end
+
+    private
+
+  def get_id
+    review_id = params[:id]
+    @review = Review.find_by_id(review_id)
   end
 
-  def new
-    @review = Review.new
-    render :new
-  end
-
-  def create
-    @review = Review.create(review_params)
-    login(@review)
-    redirect_to root_path
-    #double check this function
-  end
-
-  def show
-    render :show
-  end
-
-  def edit
-    render :edit
-  end
-
-  def update
-    review_params = get_review_params
-    review.update_attributes(review_params)
-    redirect_to show_review
-  end
-
-  def destroy
-    review.destroy
-    redirect_to root_path
-  end
-
-  private
-
-def get_id
-  review_id = params[:id]
-  @review = Review.find_by_id(review_id)
 end
