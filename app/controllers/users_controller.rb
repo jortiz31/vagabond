149162c9c3f23c0f2230423ed0876f1adf89de45
Jonathan <jortiz@tunein.com>
2016-05-05 20:12:40 -0700

    front end done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
 
     render :index
   end
+
+  def new
+    @user = User.new
+
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:username, :current_city, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get "/home", to: "home#index", as: "home"
+  # user routes
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  # post "/users", to: "users#create"
+  # get "users/:id", to: "users#show", as: "user"
+  #
+  # #sign_in/out session routes
+  # get "/login", to: "sessions#new"
+  # get "/logout", to: "sessions#destroy"
+  # post "/sessions", to: "sessions#create"
+
 end
