9ed281e7f2e72c2a47bab5415020ada8d62d3ee4
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-06 11:07:02 -0700

    removed conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,13 @@
 class UsersController < ApplicationController
 
   def index
-<<<<<<< HEAD
     @users = User.all
     render :index
-=======
-     @users = User.all
-     render :index
   end
 
   def new
     @user = User.new
     render :new
->>>>>>> e267445eb999e9a2f2572e1e317e72482aa162bd
   end
-  
+
 end
