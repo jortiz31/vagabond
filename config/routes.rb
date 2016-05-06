8d57ebe6406d0ade7c868f4f82c5bda008bb0722
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-05 23:20:55 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root   to: "users#index"        
+  root   to: "users#index",                                       as: "users"        
 ​
   get     "/users",                     to: "users#index",        as: "users"
   get     "/users/new",                 to: "users#new",          as: "new_user"
