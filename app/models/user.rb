88072e3f39a98fc65a186e5516799733da6d6c15
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-05 22:22:58 -0700

    users_controller seeds html files...code broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
+has_many : city_reviews
+has_many :cities, through: : city_reviews
+
+def self.confirm(params)
+  @user = User.find_by({email: params[:email]})
+  @user.try(:authenticate, params[:password])
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create({name: "Mononoke", date_joined: " Jan 2, 2015", current_city: "Tokyo", email: "test01@test.com"})
+User.create({name: "Dexter", date_joined: " Dec 16, 2014", current_city: "Miami", email: "test02@test.com"})
