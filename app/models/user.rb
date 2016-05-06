031741686fcc53880a6b61baeffc9d44da09af45
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-06 08:08:01 -0700

    fixing syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-has_many : city_reviews
-has_many :cities, through: : city_reviews
+  has_many :city_reviews
+  has_many :cities, through: :city_reviews
 
-def self.confirm(params)
-  @user = User.find_by({email: params[:email]})
-  @user.try(:authenticate, params[:password])
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
 end
