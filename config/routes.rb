4366b4464350eb0874efa9c54e85c4aa0d11c065
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-06 07:43:44 -0700

    fixed error in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-
-
   root   to: "users#index"
-​
   get     "/users",                     to: "users#index",        as: "users"
   get     "/users/new",                 to: "users#new",          as: "new_user"
   post    "/users",                     to: "users#create",       as: "create_user"
   get     "/users/:id",                 to: "users#show",         as: "show_user"
   get     "/users/:id/reviews",         to: "users#reviews",      as: "user_reviews"
-​
-  get     "/login",                     to: "sessions_new",       as: "new_session"
-  get     "/logout",                    to: "sessions_destroy",   as: "destroy_session"
-  post    "/sessions",                  to: "sessions_create",    as: "create_session"
-​
+  get     "/login",                     to: "sessions#new",       as: "new_session"
+  get     "/logout",                    to: "sessions#destroy",   as: "destroy_session"
+  post    "/sessions",                  to: "sessions#create",    as: "create_session"
   get     "/reviews",                   to: "reviews#index",      as: "reviews"
   get     "/reviews/new",               to: "reviews#new",        as: "new_review"
   post    "/reviews",                   to: "reviews#create",     as: "create_review"
@@ -20,11 +15,8 @@ Rails.application.routes.draw do
   get     "/reviews/:id/edit",          to: "reviews#edit",       as: "edit_review"
   patch   "/reviews/:id",               to: "reviews#update",     as: "update_review"
   delete  "/reviews/:id",               to: "reviews#destroy",    as: "destroy_review"
-​
   get     "/cities",                    to: "cities#show",        as: "all_cities"
   get     "/cities/:id",                to: "cities#index",       as: "show_city"
   get     "/cities/new",                to: "cities#new",         as: "new_city"
   post    "/cities",                    to: "cities#create",      as: "post_city"
-​
-
 end
