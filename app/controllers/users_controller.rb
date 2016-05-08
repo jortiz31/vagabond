4a2341b1e7d70359610c7e24a76cc4c7ac966557
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-08 13:18:44 -0700

    fixed user controller def create login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,14 +20,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome!"
-      redirect_to @user
-    else
-        render 'new'
-    end
-    log_in(@user)
+    login (@user)
     redirect_to @user
   end
 
