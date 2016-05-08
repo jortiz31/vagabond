53e5f47fa8c1e870c246156beb138b8087818e19
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-08 11:58:43 -0700

    added update & edit function on cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -21,6 +21,28 @@ class CitiesController < ApplicationController
   end
 
 
+
+    # show the edit city form
+    def edit
+      # get the city id from the url params
+      city_id = params[:id]
+      # use `city` to find the city in the database
+      # and save it to an instance variable
+      @city = City.find_by_id(city_id)
+      # render the edit view (it has access to instance variable)
+      render :edit
+    end
+
+
+    def update
+      city_id = params[:id]
+      city = City.find_by_id(city_id)
+      city_params = get_city_params
+      city.update_attributes(city_params)
+      redirect_to new_city
+    end
+
+
   private
     def city_params
       params.require(:city).permit(:city_name, :country)
