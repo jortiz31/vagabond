d10ef3b2f6a743d8e39eae41709d845d92b96975
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-08 21:48:11 -0700

    repaired login/logout by incorporating some code from master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,10 @@
 class UsersController < ApplicationController
-
   before_action :logged_in?, only: [:show]
+
 
   def index
     @users = User.all
     render :index
-  end
-
-  def show
-    @user = User.find(params[:id])
-    render :show
   end
 
   def new
@@ -19,39 +14,29 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome!"
-      redirect_to @user
-    else
-        render 'new'
-    end
-    log_in(@user)
+    login(@user)
     redirect_to @user
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
 
   def edit
     user_id = params[:id]
     @user = User.find_by_id(user_id)
+
     render :edit
   end
 
   def update
     user_id = params[:id]
     user = User.find_by_id(user_id)
-    user.update(user_params)
+    user.update_attributes(user_params)
+
     redirect_to user_path(user)
   end
-
-  def destroy
-    user_id = params[:id]
-    user = User.find_by_id(user_id)
-    user.destroy(user_params)
-    flash[:success] = "User deleted"
-    redirect_to users_path(users)
-  end
-
 
 
   private
